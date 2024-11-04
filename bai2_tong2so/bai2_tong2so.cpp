7cb0c6296b23a97942deb611000b76e61b199b58
MAY 06 <MAY 06@LAB2-08>
2024-11-04 14:47:12 +0700

    Add project files.

---
+++ bai2_tong2so/bai2_tong2so.cpp
@@ -0,0 +1,29 @@
+// bai2_tong2so.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "CHUONG TRINH TINH TONG HAI SO" << endly;
+	int so1, so2; // khai bao bien so1 va so2 kieu so nguyen
+	int tong;
+	cout << "nhap so1:"; cin >> so1;
+	cout << "nhap so2:"; cin >> so2:
+	tong = so1 + so2;
+	cout << "tong hai so:" so1 << " + " << so2 << "=" << tong;
+	cout << "tong hai so:" << so1 << "+" << so2 << "=" << so1 + so2;
+	return 0;
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
